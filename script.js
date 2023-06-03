59f924e272cbdf04a3360c2a413fbb66c9e9aab5
Juan.apk <105395517+jfvalera@users.noreply.github.com>
2023-06-03 11:50:31 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function descargarArchivos() {
+    // Aquí reemplaza la URL con la URL de descarga de tu archivo alojado en tu servidor web
+    var url = "https://drive.google.com/file/d/1xyKFHEJWH0pzwb8UuH3uQdUayofzimC9/view?usp=sharing";
+    window.location.href = url;
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
